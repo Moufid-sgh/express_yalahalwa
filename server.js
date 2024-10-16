b1e00d233d46995a328732c49bb89a87e5836558
Moufid <moufid.sgh@protonmail.com>
2024-10-16 14:48:21 +0100

    first

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 4000; 
-app.use('/public', express.static(path.join(__dirname, 'assets')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
